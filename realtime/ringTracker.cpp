e196802300aa633ab6cf27948ed9733e38747f14
peng <605144885@qq.com>
2018-03-28 16:22:34 +0800

    init realtime-coordinate

---
+++ realtime/ringTracker.cpp
@@ -0,0 +1,5 @@
+//
+// Created by peng on 18-3-28.
+//
+
+#include "ringTracker.h"
---
+++ realtime/ringTracker.h
@@ -0,0 +1,14 @@
+//
+// Created by peng on 18-3-28.
+//
+
+#ifndef REALTIME_RINGTRACKER_H
+#define REALTIME_RINGTRACKER_H
+
+
+class ringTracker {
+
+};
+
+
+#endif //REALTIME_RINGTRACKER_H
